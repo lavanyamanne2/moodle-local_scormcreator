fa2d062167dbb162450416a908ec700399627087
Lavanya Manne <lavanyamanne2@users.noreply.github.com>
2023-03-05 18:12:46 +0530

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022112801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022112805;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022111800;        // Requires this Moodle version.
 $plugin->release = '4.1';
 $plugin->maturity = MATURITY_STABLE;
